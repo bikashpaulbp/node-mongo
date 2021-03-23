42d88b91ac9313589bc77acb84a44f0488760812
bikashpaulbp <bikashpaulbp92@gmail.com>
2021-03-23 22:54:34 +0600

    done

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(cors());
+
+app.use(bodyParser.json());
+
+const name = ['suhana', 'karim', 'rahim', 'jodu', 'rodu', 'fodu', 'hobu', 'luku'];
+
 app.get('/', (req, res) => {
     res.send('Thank you for calling me')
+})
+
+app.get('/abaal/:id', (req, res) => {
+    const id = req.params.id;
+    const user = name[id];
+    res.send({id, user})
+})
+
+
+app.post('/addUser', (req, res) => {
+    const user = req.body;
+    user.id = 7;
+    res.send(req.body)
 })
 
 app.listen(3000, () => console.log("Listening to port 3000"))
